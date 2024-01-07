4bf54b8dff1493626b853ab40b44ee914e07e0bd
unknown <bishow712@gmail.com>
2024-01-07 18:23:45 +0545

    GetPokemons

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    // Attributes = [....]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+
+        // Constructor (ctor)
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemonsFromDatabase();
+            
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ pokemontest/PokemonControllerTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PokemonReviewApp.Controllers;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace pokemontest;
+
+[TestClass]
+public class PokemonControllerTests
+{
+    Mock<IPokemonRepository> mockPokemonRepo = new Mock<IPokemonRepository>();
+
+    [TestMethod]
+    public void GetPokemons_Returns_OkObjectResult_WhenDatabaseFetchSucceeds()
+    {
+        mockPokemonRepo.Setup(repo => repo.GetPokemonsFromDatabase())
+               .Returns(new List<Pokemon>());
+
+         var pokemonController = new PokemonController(mockPokemonRepo.Object);
+
+        Assert.IsInstanceOfType<OkObjectResult>(pokemonController.GetPokemons());
+    }
+}
